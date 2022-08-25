72bca866071473fd04ea8ddf2577fef3b3c95ec4
53911453+brianpavon@users.noreply.github.com <53911453+brianpavon@users.noreply.github.com>
2022-08-25 20:26:57 -0300

    primeros archivos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { LoginComponent } from './pages/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ErrorComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
