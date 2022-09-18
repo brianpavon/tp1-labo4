ff53eddc3e8fb20942622cf42634ea1034fe966e
53911453+brianpavon@users.noreply.github.com <53911453+brianpavon@users.noreply.github.com>
2022-09-17 21:04:02 -0300

    agrego menu de juegos

--- src/app/juegos/pages/menu-juegos/menu-juegos.component.ts
+++ src/app/juegos/pages/menu-juegos/menu-juegos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-menu-juegos',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./menu-juegos.component.css']
 })
 export class MenuJuegosComponent implements OnInit {
-
-  constructor() { }
+  gamesOn:boolean;
+  userLogged:boolean;
+  constructor(private user:UserService,private auth:AngularFireAuth) {
+    this.gamesOn = false;
+    this.userLogged = false;
+    auth.authState.subscribe(user=>{
+      user ? this.userLogged = true : this.userLogged = false;
+      user ? this.gamesOn = true : this.gamesOn = false;
+    })
+     
+  }
 
   ngOnInit(): void {
+  }
+  activateGames(status:boolean){
+    status  ? this.gamesOn = true : this.gamesOn = false;    
   }
 
 }
