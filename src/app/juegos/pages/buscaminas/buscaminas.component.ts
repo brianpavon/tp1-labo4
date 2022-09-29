171d1da7e618b78842b06f9ee42ad4d2c4a96200
53911453+brianpavon@users.noreply.github.com <53911453+brianpavon@users.noreply.github.com>
2022-09-29 02:20:53 -0300

    ok sprint 4

--- src/app/juegos/pages/buscaminas/buscaminas.component.ts
+++ src/app/juegos/pages/buscaminas/buscaminas.component.ts
@@ -1,249 +1,274 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
+import Swal from 'sweetalert2';
 
 
 let arrayPiezzasGlobal :any;
-let numPiezasDescubiertas : any;
-let scorePlayer = 0;
+let numPiezasDescubiertas : number = 0;
 
 @Component({
   selector: 'app-buscaminas',
   templateUrl: './buscaminas.component.html',
   styleUrls: ['./buscaminas.component.css']
 })
-export class BuscaminasComponent {
-  // Variables
-	title = 'BuscaminasAngular';
-	namePlayer = "Sin nombre";
-	scorePlayer = "0";
-	timePlayer = "00:00";
-	dificultadPlayer = 0;
-	contenido: String = "";
-	arrayPiezas = [];
+export class BuscaminasComponent {      
+      dificultadPlayer = 0;
+      contenido: String = "";
+      arrayPiezas = [];
 
-	// Link
-	@ViewChild('contenido') contenedor!: ElementRef;
-	@ViewChild('btnIniciar') btnIniciar!: ElementRef;
-	@ViewChild('configuracion') panelConfiguracion!: ElementRef;
-	//@ViewChild('botonera') panelBotonera!: ElementRef;
+      // Link
+      @ViewChild('contenido') contenedor!: ElementRef;
+      @ViewChild('btnIniciar') btnIniciar!: ElementRef;
+      @ViewChild('configuracion') panelConfiguracion!: ElementRef;
+      @ViewChild('reset') btnReset!: ElementRef;
 
-	/**
-	 * Muestra la configuración inicial del juego
-	 */
-	MostrarConfiguracion() {
-		this.btnIniciar.nativeElement.style.visibility = "hidden";
-		this.panelConfiguracion.nativeElement.style.visibility = "visible";
-	}
+      resetJuego(){
+        this.btnReset.nativeElement.style.visibility = "hidden";
+        this.contenedor.nativeElement.removeChild(document.getElementById('tabla'));
+        this.panelConfiguracion.nativeElement.style.visibility = "visible";
+      }
 
-	StartGameEvent(dificultad: number) {
-		this.panelConfiguracion.nativeElement.style.visibility = "hidden";
-		//this.panelBotonera.nativeElement.style.visibility = "visible";
+      mostrarConfiguracion() {
+        this.btnIniciar.nativeElement.style.visibility = "hidden";
+        this.panelConfiguracion.nativeElement.style.visibility = "visible";
+      }
 
-		let piezas = 0;
-		let bombas = 0;
+      iniciarJuego(dificultad: number) {
+        this.panelConfiguracion.nativeElement.style.visibility = "hidden";
+        //this.panelBotonera.nativeElement.style.visibility = "visible";
 
-		switch (dificultad) {
-			case 0:
-				// Facil
-				piezas = 25;
-				bombas = 5;
-				break;
-			case 1:
-				// Normal
-				piezas = 49;
-				bombas = 15;
-				break;
-			case 2:
-				// Dificil
-				piezas = 64;
-				bombas = 40;
-				break;
-		}
+        let piezas = 0;
+        let bombas = 0;
 
-		// Inicializamos Array
-		let arrayPiezas = new Array(piezas);
-		arrayPiezas = this.colocarBombas(arrayPiezas, bombas);
-		arrayPiezas = this.checkBombs(arrayPiezas, piezas);
-		console.log("RESULTADO");
-		console.log(arrayPiezas);
+        switch (dificultad) {
+          case 0:
+            // Facil
+            piezas = 25;
+            bombas = 5;
+            break;
+          case 1:
+            // Normal
+            piezas = 49;
+            bombas = 15;
+            break;
+          case 2:
+            // Dificil
+            piezas = 64;
+            bombas = 30;
+            break;
+        }
 
-		// Guardamos partida actual en Global (Para acceder desde el Controladorcelda)
-		arrayPiezzasGlobal = arrayPiezas;
-
-		// Oculta botón Iniciar partida
-		this.btnIniciar.nativeElement.style.display = "none";
-
-		this.PreparePlantilla(arrayPiezas);
-	}
-
-	/**
-	 * Metodo prepara la interfaz de la tabla
-	 * @param {array} arrayPiezas Recibe el número de piezas que se mostraran
-	 */
-	PreparePlantilla(arrayPiezas:number[]) {
-		let cuadrado = Math.sqrt(arrayPiezas.length);
-
-		var tableBase = document.createElement('table');
-		var tbody = document.createElement('tbody');
-		var numPieza = 0;
-
-		for (var i = 0; i < cuadrado; i++) {
-			var fila = document.createElement('tr');
-
-			for (var c = 0; c < cuadrado; c++) {
-				var columna = document.createElement('td');
-				columna.id = "piece" + numPieza;
-				columna.addEventListener("click", ControladorCelda);
-				columna.style.backgroundImage = "url(\"assets/images/Bloque1.gif\")"
-				columna.style.backgroundSize = "contain";
-        columna.classList.add('columna');
-
-				numPieza++;
-				fila.appendChild(columna);
-			}
-
-			tbody.appendChild(fila);
-		}
-		tableBase.appendChild(tbody);
-		this.contenedor.nativeElement.appendChild(tableBase);
-	}
-
-	/**
-	 * Metodo realiza la colocación aleatoria de las bombas
-	 * @param {array} arrayPiezas 
-	 * @param {int} numBombas 
-	 */
-	colocarBombas(arrayPiezas:number[], numBombas:number) {
-
-		// Generar posicones de bombas
-    //let fruits: string[]
-		let posicionesbombas : number[] = [];
-		for (var i = 0; i < numBombas; i++) {
-			let aleatorio = -1;
-			var valido = false;
-
-			while (!valido) {
-				aleatorio = Math.floor((Math.random() * arrayPiezas.length));
-        console.log(posicionesbombas);
+        // Inicializamos Array
+        let arrayPiezas = new Array(piezas);        
+        //console.log(arrayPiezas);
         
-				if (!posicionesbombas.includes(aleatorio)) {
-					valido = true;
-				}
-			}
-			posicionesbombas.push(aleatorio);
-		}
+        arrayPiezas = this.colocarBombas(arrayPiezas, bombas);
+        
+        arrayPiezas = this.checkBombas(arrayPiezas, piezas);
+        //console.log("RESULTADO");
+        //console.log(arrayPiezas);
 
-		console.log("Coloco bombas en las posiciones: " + posicionesbombas);
+        // Guardamos partida actual en Global (Para acceder desde el Controladorcelda)
+        arrayPiezzasGlobal = arrayPiezas;
 
-		// Colocar bombas
-		for (var i = 0; i < posicionesbombas.length; i++) {
-			arrayPiezas[posicionesbombas[i]] = -1;
-		}
+        // Oculta botón Iniciar partida
+        this.btnIniciar.nativeElement.style.display = "none";
 
-		return arrayPiezas;
-	}
+        this.armarCampoDeMinas(arrayPiezas);
+      }
 
-	// /**
-	//  * Metodo devuelve en que cordenada se encuentra una pieza
-	//  * @param {int} numPieza Número de la pieza del puzzle
-	//  * @param {int} totalPiezas Número total de piezzas del puzle
-	//  * @returns {array} Número Fila x Columna-
-	//  */
-	// cordenadaPieza(numPieza, totalPiezas) {
-	// 	let cuadrado = Math.sqrt(totalPiezas);
+      
+      armarCampoDeMinas(arrayPiezas:number[]) {
+        let cuadrado = Math.sqrt(arrayPiezas.length);
 
-	// 	// Calculamos la fila y la columna
-	// 	let fila = Math.round(numPieza / cuadrado);
-	// 	let columna = Math.round(numPieza % cuadrado);
+        let tableBase = document.createElement('table');
+        tableBase.setAttribute("id","tabla");
+        let tbody = document.createElement('tbody');
+        let numPieza = 0;
 
-	// 	let posicion = [];
-	// 	posicion[0] = fila;   // FILA ~ Alto
-	// 	posicion[1] = columna;   // COLUMNA ~ Ancho
+        for (let i = 0; i < cuadrado; i++) {
+          let fila = document.createElement('tr');
 
-	// 	return posicion;
-	// }
+          for (let c = 0; c < cuadrado; c++) {
+            let columna = document.createElement('td');
+            columna.id = "pieza" + numPieza;
+            columna.addEventListener("click", ControladorCelda);
+            columna.style.backgroundImage = "url(\"assets/images/Bloque1.gif\")"
+            columna.style.backgroundSize = "contain";
+            columna.classList.add('columna');
+
+            numPieza++;
+            fila.appendChild(columna);
+          }
+
+          tbody.appendChild(fila);
+        }
+        tableBase.appendChild(tbody);
+        this.contenedor.nativeElement.appendChild(tableBase);
+      }
+      
+      colocarBombas(arrayPiezas:number[], numBombas:number) {
+
+        // Generar posicones de bombas
+        //let fruits: string[]
+        let posicionesbombas : number[] = [];
+        for (let i = 0; i < numBombas; i++) {
+          let aleatorio = -1;
+          let valido = false;
+
+          while (!valido) {
+            aleatorio = Math.floor((Math.random() * arrayPiezas.length));
+            //console.log(aleatorio);
+            
+            if (!posicionesbombas.includes(aleatorio)) {
+              valido = true;
+            }
+          }
+          posicionesbombas.push(aleatorio);
+        }
+
+        console.log("Coloco bombas en las posiciones: " + posicionesbombas);
+
+        // Colocar bombas
+        for (let i = 0; i < posicionesbombas.length; i++) {
+          arrayPiezas[posicionesbombas[i]] = -1;
+        }
+
+        return arrayPiezas;
+      }      
+
+      checkBombas(arrayPiezas:number[], numPiezas:number) {
+        // Comenzamos comprobación
+        let cuadrado = Math.sqrt(numPiezas);
+
+        // Analizamos cada pieza 
+        for (let i = 0; i < arrayPiezas.length; i++) {
+          let contadorBomba = 0;
+
+          // Descartamos que sea bomba
+          if (arrayPiezas[i] != -1) {
+            // No hay bomba 
+            let izquierda: boolean = true; 
+            let derecha: boolean = true;
+            let arriba: boolean = true;
+            let abajo: boolean = true;
+
+            // Habilitamos izquierda ?
+            //console.log(`i ${i} cuadrado es ${cuadrado}`);
+            
+            if (Math.floor(i / cuadrado) != Math.floor((i - 1) / cuadrado)) {
+              izquierda = false;
+              //console.log("#" + i + " Izquierda desactivado\n" + Math.floor(i / cuadrado) + " vs " + Math.floor((i - 1) / cuadrado));
+            }
+
+            // Habilitamos derecha ?
+            if (Math.floor(i / cuadrado) != Math.floor((i + 1) / cuadrado)) {
+              derecha = false;
+              //console.log("#" + i + " Derecha desactivado\n" + Math.floor(i / cuadrado) + " vs " + Math.floor((i + 1) / cuadrado));
+            }
+
+            // Habilitamos arriba ?
+            if ((i - cuadrado) == undefined) {
+              arriba = false;
+              //console.log("#" + i + " Arriba desactivado");
+            }
+
+            // Habilitamos abajo ?
+            if ((i + cuadrado) == undefined) {
+              abajo = false;
+              //console.log("#" + i + " Abajo desactivado");
+            }
+
+            // Checkea en todas las direcciones habilitadas
+
+            // Izquierda
+            if (izquierda && arrayPiezas[(i - 1)] == -1) {               
+              contadorBomba++ 
+            }
+            // Derecha
+            if (derecha && arrayPiezas[(i + 1)] == -1) {               
+              contadorBomba++ 
+            }
+            // Arriba
+            if (arriba && arrayPiezas[(i - cuadrado)] == -1) { 
+              contadorBomba++               
+            }
+            // Arriba Izquierda
+            if (arriba && izquierda && arrayPiezas[(i - cuadrado - 1)] == -1) { 
+              contadorBomba++               
+            }
+            // Arriba Derecha
+            if (arriba && derecha && arrayPiezas[(i - cuadrado + 1)] == -1) { 
+              contadorBomba++               
+            }
+            // Abajo
+            if (abajo && arrayPiezas[(i + cuadrado)] == -1) { 
+              contadorBomba++               
+            }
+            // Abajo Izquierda
+            if (abajo && izquierda && arrayPiezas[(i + cuadrado - 1)] == -1) { 
+              contadorBomba++               
+            }
+            // Abajo Derecha
+            if (abajo && derecha && arrayPiezas[(i + cuadrado + 1)] == -1) { 
+              contadorBomba++              
+            }
+          } else {
+            // Es la bomba :O
+            contadorBomba = -1;
+          }
+          arrayPiezas[i] = contadorBomba;
+          //console.log(`${contadorBomba} + ${arrayPiezas[i]}`);
+        }
+
+        return arrayPiezas;
+      }
+
+      
 
 
-	/**
-	 * EL metodo comprueba cuantas bombas hay alrededor de cada pieza y lo registra con un numero.
-	 * @param numPiezas Array con las piezzas
-	 * @returns {Array} Devuelve un array de 2 dimensiones con todas los valores resultantes
-	 */
-	checkBombs(arrayPiezas:number[], numPiezas:number) {
-		// Comenzamos comprobación
-		let cuadrado = Math.sqrt(numPiezas);
+}
 
-		// Analizamos cada pieza 
-		for (var i = 0; i < arrayPiezas.length; i++) {
-			var contadorBomba = 0;
+function ganaste(){
+  numPiezasDescubiertas = 0
+  
+  Swal.fire({
+      position: 'center',
+      title: 'Ganaste!!',
+      imageUrl: 'assets/images/happy.png',
+      imageWidth: 200,
+      imageHeight: 200,
+      imageAlt: 'Custom image',      
+  });
+    //@ts-ignore    
+  document.getElementById('reset').style.visibility = "visible";
+  
+}
 
-			// Descartamos que sea bomba
-			if (arrayPiezas[i] != -1) {
-				// No hay bomba bro
-				var izquierda: boolean = true, derecha: boolean = true, arriba: boolean = true, abajo: boolean = true;
-
-				// Habilitamos izquierda ?
-				if (Math.floor(i / cuadrado) != Math.floor((i - 1) / cuadrado)) {
-					izquierda = false;
-					console.log("#" + i + " Izquierda desactivado\n" + Math.floor(i / cuadrado) + " vs " + Math.floor((i - 1) / cuadrado));
-				}
-
-				// Habilitamos derecha ?
-				if (Math.floor(i / cuadrado) != Math.floor((i + 1) / cuadrado)) {
-					derecha = false;
-					console.log("#" + i + " Derecha desactivado\n" + Math.floor(i / cuadrado) + " vs " + Math.floor((i + 1) / cuadrado));
-				}
-
-				// Habilitamos arriba ?
-				if ((i - cuadrado) == undefined) {
-					arriba = false;
-					console.log("#" + i + " Arriba desactivado");
-				}
-
-				// Habilitamos abajo ?
-				if ((i + cuadrado) == undefined) {
-					abajo = false;
-					console.log("#" + i + " Abajo desactivado");
-				}
-
-				// Checkea en todas las direcciones habilitadas
-
-				// Izquierda
-				if (izquierda && arrayPiezas[(i - 1)] == -1) { contadorBomba++ }
-				// Derecha
-				if (derecha && arrayPiezas[(i + 1)] == -1) { contadorBomba++ }
-				// Arriba
-				if (arriba && arrayPiezas[(i - cuadrado)] == -1) { contadorBomba++ }
-				// Arriba Izquierda
-				if (arriba && izquierda && arrayPiezas[(i - cuadrado - 1)] == -1) { contadorBomba++ }
-				// Arriba Derecha
-				if (arriba && derecha && arrayPiezas[(i - cuadrado + 1)] == -1) { contadorBomba++ }
-				// Abajo
-				if (abajo && arrayPiezas[(i + cuadrado)] == -1) { contadorBomba++ }
-				// Abajo Izquierda
-				if (abajo && izquierda && arrayPiezas[(i + cuadrado - 1)] == -1) { contadorBomba++ }
-				// Abajo Derecha
-				if (abajo && derecha && arrayPiezas[(i + cuadrado + 1)] == -1) { contadorBomba++ }
-			} else {
-				// Es la bomba :O
-				contadorBomba = -1;
-			}
-
-			arrayPiezas[i] = contadorBomba;
-		}
-
-		return arrayPiezas;
-	}
-
-
+function perdiste(){
+  numPiezasDescubiertas = 0
+  setTimeout(() => {    
+    Swal.fire({
+      position: 'center',
+      title: 'Perdiste!',      
+      imageUrl: 'assets/images/dead.png',
+      imageWidth: 200,
+      imageHeight: 200,
+      imageAlt: 'Custom image',
+    });
+    //@ts-ignore    
+    document.getElementById('reset').style.visibility = "visible";
+  }, 2200);
+    
+  
 }
 
 /**
  * Controlador de la piezza con sus metodos y comprobaciones
  */
- function ControladorCelda(this:any) {
-	console.log(this.id);
+function ControladorCelda(this:any) {
+	//console.log(this.id);
 	// Obtenemos número de la pieza
-	let numeroPieza = this.id.replace("piece", "");
+	let numeroPieza = this.id.replace("pieza", "");
 
 	// BOMBA
 	let listadoBombas = [];
@@ -261,9 +286,9 @@ export class BuscaminasComponent {
 		// Aplicar bomba a las piezas
 		for (let a = 0; a < listadoBombas.length; a++) {
       //@ts-ignore
-      document.getElementById("piece" + listadoBombas[a]).style.backgroundImage = "url(\"assets/images/explosion.gif\")";
+      document.getElementById("pieza" + listadoBombas[a]).style.backgroundImage = "url('assets/images/explosion.gif')";
       //@ts-ignore
-      document.getElementById("piece" + listadoBombas[a]).style.backgroundSize = "contain";
+      document.getElementById("pieza" + listadoBombas[a]).style.backgroundSize = "contain";
       
 		}
 
@@ -272,27 +297,24 @@ export class BuscaminasComponent {
 		for (let i = 0; i < celdas.length; i++) {
 			celdas[i].removeEventListener("click", ControladorCelda);
 		}
-
-		// Fin
-		// document.getElementsByClassName("neon")[0].textContent = "Has";
-		// document.getElementsByClassName("flux")[0].textContent = "Perdido";
+    perdiste();
+		// Fin		
 	} else {
-		scorePlayer+= arrayPiezzasGlobal[numeroPieza] * 2;
-    //@ts-ignore
-		//document.getElementById("puntuacion").textContent = "Puntuación: " + scorePlayer;	
 
 		this.textContent = arrayPiezzasGlobal[numeroPieza];
-		this.style.backgroundImage = "url(\"assets/images/bloque2.png\")"
+		this.style.backgroundImage = "url('assets/images/bloque2.png')"
 		this.style.backgroundSize = "contain";
 		this.style.fontSize = "2em";
-
+    //console.log(numPiezasDescubiertas);    
+    numPiezasDescubiertas++;
+    //console.log(numPiezasDescubiertas);
 		// Comprobamos que ha ganado
 		if (numPiezasDescubiertas == (arrayPiezzasGlobal.length - listadoBombas.length)){
-			// Ya ha ganado
-			// document.getElementsByClassName("neon")[0].textContent = "Has";
-			// document.getElementsByClassName("flux")[0].textContent = "Ganado";	
-      // //@ts-ignore
-			// document.getElementById("puntuacion").innerHTML = "Fin | " + scorePlayer;	
+      let celdas = document.getElementsByTagName("td");
+      for (let i = 0; i < celdas.length; i++) {
+        celdas[i].removeEventListener("click", ControladorCelda);
+      }
+      ganaste();
 		}
 	}
 
